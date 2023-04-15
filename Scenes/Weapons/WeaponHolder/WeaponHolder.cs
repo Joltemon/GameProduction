7f1d146bef2e5197e6a8b68728b22b440a39f87e
squidink7 <squidinkrf@gmail.com>
2023-04-15 22:05:26 +0930

    gun animations!

--- Scenes/Weapons/WeaponHolder/WeaponHolder.cs
+++ Scenes/Weapons/WeaponHolder/WeaponHolder.cs
@@ -4,16 +4,38 @@ using System;
 public partial class WeaponHolder : Node3D
 {
 	[Export] Propulsor? Weapon;
+	[Export] Node3D? WeaponOffset;
 	[Export] RayCast3D? AimRaycast;
+	[Export] AnimationPlayer? AnimationPlayer;
+
+	Vector2 mouseVelocity;
 
 	public override void _Input(InputEvent inputEvent)
 	{
+		// Shooting the gun
 		if (inputEvent.IsActionPressed("PrimaryFire"))
 		{
 			if (AimRaycast!.IsColliding() && GlobalPosition.DistanceTo(AimRaycast.GetCollisionPoint()) <= 1)
 				CloseRangePrimaryFire();
 			else
 				StandardPrimaryFire();
+		}
+
+		if (inputEvent is InputEventMouseMotion mouseEvent)
+		{
+			mouseVelocity = mouseEvent.Relative;
+		}
+	}
+
+	public override void _Process(double delta)
+	{
+		if (WeaponOffset != null)
+		{
+			// Look swaying
+			var targetRotation = new Vector3();
+			targetRotation.X += mouseVelocity.Y / 256;
+			targetRotation.Y += mouseVelocity.X / 256;
+			WeaponOffset.Rotation = WeaponOffset.Rotation.Lerp(targetRotation, 0.2f);
 		}
 	}
 
@@ -31,6 +53,7 @@ public partial class WeaponHolder : Node3D
 	
 	void StandardPrimaryFire()
 	{
+		AnimationPlayer?.Play("Fire");
 		if (AimRaycast!.IsColliding())
 		{
 			Weapon!.Target = AimRaycast.GetCollisionPoint();
