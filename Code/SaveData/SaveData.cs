4014545eac1d4a503b6cfe58dbb47932c52dda0a
Rafael Fryer <squidinkrf@gmail.com>
2023-05-01 20:27:17 +0930

    Save data backend

--- Code/SaveData/SaveData.cs
+++ Code/SaveData/SaveData.cs
@@ -1,10 +1,50 @@
 using Godot;
-using System;
+using Tomlyn;
+using System.IO;
+using System.Collections.Generic;
 
 class Savedata
 {
-	public static void Init()
+	static Dictionary<string, Variant> SettingsData = new();
+	
+	public static void Load()
 	{
-		// GD.Print(ProjectSettings.GlobalizePath("user://"));
+		var settingsPath = ProjectSettings.GlobalizePath("user://settings.save");
+		var settingsString = File.ReadAllText(settingsPath);
+		SettingsData = Toml.ToModel<Dictionary<string, Variant>>(settingsString);
+	}
+	
+	public static T Get<[MustBeVariant] T>(string name, T defaultValue)
+	{
+		T result;
+
+		if (SettingsData.ContainsKey(name))
+		{
+			result = SettingsData[name].As<T>();
+		}
+		else
+		{
+			result = defaultValue;
+		}
+		return result;
+	}
+
+	public static void Set(string name, Variant value)
+	{
+		if (SettingsData.ContainsKey(name))
+		{
+			SettingsData[name] = value;
+		}
+		else
+		{
+			SettingsData.Add(name, value);
+		}
+	}
+
+	public static void Save()
+	{
+		var settingsPath = ProjectSettings.GlobalizePath("user://settings.save");
+		var settingsString = Toml.FromModel(SettingsData);
+		File.WriteAllText(settingsPath, settingsString);
 	}
 }
