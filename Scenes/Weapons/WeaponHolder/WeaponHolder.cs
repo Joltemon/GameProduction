e8092cd35d226ac17bc847332f0806a6b1e4b72d
Rafael Fryer <squidinkrf@gmail.com>
2023-05-03 18:38:20 +0930

    Reloading

--- Scenes/Weapons/WeaponHolder/WeaponHolder.cs
+++ Scenes/Weapons/WeaponHolder/WeaponHolder.cs
@@ -34,6 +34,21 @@ public partial class WeaponHolder : Node3D
 		}
 	}
 
+	public void AddAmmunition(int amount)
+	{
+		if (Weapon != null)
+			Weapon.Ammunition += amount;
+	}
+
+	public void SetAmmunition(int amount)
+	{
+		if (Weapon != null)
+		{
+			Weapon.Ammunition = amount;
+			OnAmmunitionUpdated(Weapon.Ammunition);
+		}
+	}
+
 	public override void _Process(double delta)
 	{
 		if (WeaponOffset != null)
