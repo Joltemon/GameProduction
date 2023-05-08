31e79d574ebb3fb3826f401fcdd3b012b41b30c8
Tabby <113022918+Joltemon@users.noreply.github.com>
2023-05-08 18:27:22 +0930

    roadssssssssssssssssssssssssssssssssssss

--- Assets/Models/Collectables/EnergyCrystal/EnergyCrystal.cs
+++ Assets/Models/Collectables/EnergyCrystal/EnergyCrystal.cs
@@ -17,7 +17,7 @@ public partial class EnergyCrystal : Node3D
 	// Called when the node enters the scene tree for the first time.
 	public override void _Ready()
 	{
-		
+		GD.Print(AmmoAmount);
 	}
 
 	// Called every frame. 'delta' is the elapsed time since the previous frame.
