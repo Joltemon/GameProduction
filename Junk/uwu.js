8b4a6173052a2ee0a6f351e921dd0cde6e9f6eaa
Lane Malpas <lanecmalpas@gmail.com>
2023-06-21 17:56:51 +0930

    even moire

--- Junk/uwu.js
+++ Junk/uwu.js
@@ -1,8 +1,1 @@
-// :)
-
-
 let string1 = "haha now there javascript";
-let string2 = "pls don't delete this";
-let string3 = "that would make me very sad";
-let deleteproject = 100;
-let godotExists = false;
